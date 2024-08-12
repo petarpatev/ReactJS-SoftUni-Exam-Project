0ffec9e434ef6349601887d979b4cff25e91ddc4
Petar Patev <petarpatev@abv.bg>
2024-08-12 12:08:21 +0300

    Update comment form validation

--- client/src/components/comments/CommentForm.jsx
+++ client/src/components/comments/CommentForm.jsx
@@ -2,6 +2,7 @@ import { useState, useContext } from "react"
 import { userContext } from "../../contexts/user"
 
 import * as commentService from "../../api/comments"
+import { isValid } from "../../utils/validation";
 
 export default function CommentForm({ articleId, onAddComment }) {
 
@@ -9,7 +10,7 @@ export default function CommentForm({ articleId, onAddComment }) {
 
     const [commentValues, setCommentValues] = useState({
         comment: '',
-        author: ''
+        // author: ''
     })
 
     const changeValueHandler = (e) => {
@@ -21,24 +22,29 @@ export default function CommentForm({ articleId, onAddComment }) {
 
     const submitHandler = async (e) => {
         e.preventDefault();
-        try {
-            const newComment = await commentService.create({
-                articleId: articleId,
-                comment: commentValues.comment,
-                author: user.email
-            })
+        if (isValid(commentValues)) {
+            try {
+                const newComment = await commentService.create({
+                    articleId: articleId,
+                    comment: commentValues.comment,
+                    author: user.email
+                })
 
-            onAddComment(newComment);
-            e.target.reset();
-            setCommentValues(state => ({
-                ...state,
-                comment: "",
-                author: ""
-            }))
-        } catch (err) {
-            console.error("Failed to create comment:", err);
-            alert("Failed to create comment! Please try again!");
+                onAddComment(newComment);
+                e.target.reset();
+                setCommentValues(state => ({
+                    ...state,
+                    comment: "",
+                    // author: ""
+                }))
+            } catch (err) {
+                console.error("Failed to create comment:", err);
+                alert("Failed to create comment! Please try again!");
+            }
+        } else {
+            alert("All fields are requeired")
         }
+
     }
 
     return (
